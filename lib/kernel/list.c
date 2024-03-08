804b6f7ed73fa5bbc185fb86a71c4e692442647b
won0845 <tpqms775@gmail.com>
2024-03-08 23:48:34 +0900

    list 주석 추가

--- lib/kernel/list.c
+++ lib/kernel/list.c
@@ -8,15 +8,15 @@
 
    An empty list looks like this:
 
-   +------+     +------+
-   <---| head |<--->| tail |--->
-   +------+     +------+
+   			+------+     +------+
+   null <---| head |<--->| tail |---> null
+   			+------+     +------+
 
    A list with two elements in it looks like this:
 
-   +------+     +-------+     +-------+     +------+
-   <---| head |<--->|   1   |<--->|   2   |<--->| tail |<--->
-   +------+     +-------+     +-------+     +------+
+ 			+------+     +-------+     +-------+     +------+
+   null <---| head |<--->|   1   |<--->|   2   |<--->| tail |<---> null
+   			+------+     +-------+     +-------+     +------+
 
    The symmetry of this arrangement eliminates lots of special
    cases in list processing.  For example, take a look at
@@ -31,32 +31,50 @@
    operations, which can be valuable.)
 */
 
+/*사용하지 않음*/
 static bool is_sorted(struct list_elem *a, struct list_elem *b,
-					  list_less_func *less, void *aux) UNUSED;
+ list_less_func *less, void *aux) UNUSED;
 
-/* Returns true if ELEM is a head, false otherwise. */
+/* 
+* Returns true if ELEM is a head, false otherwise. 
+* elem이 head면 true를 반환하고, 그렇지 않으면 false를 반환합니다.	
+*/
 static inline bool
 is_head(struct list_elem *elem)
 {
 	return elem != NULL && elem->prev == NULL && elem->next != NULL;
 }
 
-/* Returns true if ELEM is an interior element,
-   false otherwise. */
+/* Returns true if ELEM is an interior element, false otherwise. 
+* elem이 interior이면 true를 반환하고, 그렇지 않으면 false를 반환합니다.
+* head와 tail 사이에 있는 경우
+*/
 static inline bool
 is_interior(struct list_elem *elem)
 {
 	return elem != NULL && elem->prev != NULL && elem->next != NULL;
 }
 
-/* Returns true if ELEM is a tail, false otherwise. */
+/* Returns true if ELEM is a tail, false otherwise. 
+* elem이 tail이면 true를 반환하고, 그렇지 않으면 false를 반환합니다.
+* elem이 null이 아니고 앞은 존재하는데 다음이 NULL 인경우 tail
+*/
 static inline bool
 is_tail(struct list_elem *elem)
 {
 	return elem != NULL && elem->prev != NULL && elem->next == NULL;
 }
 
-/* Initializes LIST as an empty list. */
+/* Initializes LIST as an empty list. 
+* LIST를 빈 리스트로 초기화합니다.
+* 리스트의 head와 tail을 초기화합니다.
+* head의 prev는 NULL, next는 tail을 가리키고
+* tail의 prev는 head를 가리키고 next는 NULL을 가리킵니다.
+* 리스트의 head와 tail은 리스트의 시작과 끝을 나타냅니다.
+* 리스트의 시작과 끝에는 head와 tail이 있습니다.
+* head는 리스트의 첫 번째 요소 앞에 있고, tail은 마지막 요소 뒤에 있습니다.
+* head의 prev 링크와 tail의 next 링크는 null입니다.	
+*/
 void list_init(struct list *list)
 {
 	ASSERT(list != NULL);
@@ -66,7 +84,10 @@ void list_init(struct list *list)
 	list->tail.next = NULL;
 }
 
-/* Returns the beginning of LIST.  */
+/* Returns the beginning of LIST.  
+* LIST의 시작을 반환합니다.
+* 리스트가 가르키고 있는 헤드의 다음 요소 즉 첫번째 요소를 반환한다.
+*/
 struct list_elem *
 list_begin(struct list *list)
 {
@@ -75,8 +96,14 @@ list_begin(struct list *list)
 }
 
 /* Returns the element after ELEM in its list.  If ELEM is the
-   last element in its list, returns the list tail.  Results are
-   undefined if ELEM is itself a list tail. */
+*  last element in its list, returns the list tail.  Results are
+*  undefined if ELEM is itself a list tail. 
+* elem의 다음 요소를 반환합니다. elem이 리스트의 마지막 요소인 경우 리스트의 tail을 반환합니다. 
+* elem이 자체적으로 리스트 tail인 경우 결과는 정의되지 않습니다.   
+* elem이 head이거나 interior인 경우 elem의 next를 반환합니다.
+* elem이 NULL이 아거나 tail인 경우 NULL을 반환합니다.
+* elem이 NULL이 아거나 head인 경우 elem의 next를 반환합니다.   
+*/
 struct list_elem *
 list_next(struct list_elem *elem)
 {
@@ -85,10 +112,12 @@ list_next(struct list_elem *elem)
 }
 
 /* Returns LIST's tail.
-
-   list_end() is often used in iterating through a list from
-   front to back.  See the big comment at the top of list.h for
-   an example. */
+*   list_end() is often used in iterating through a list from
+*   front to back.  See the big comment at the top of list.h for
+*   an example. 
+* 리스트의 tail을 반환합니다.
+*   
+*/
 struct list_elem *
 list_end(struct list *list)
 {
@@ -97,7 +126,9 @@ list_end(struct list *list)
 }
 
 /* Returns the LIST's reverse beginning, for iterating through
-   LIST in reverse order, from back to front. */
+   LIST in reverse order, from back to front. 
+* tail이 아닌 맨 마지막 요소를 반환한다.
+*/
 struct list_elem *
 list_rbegin(struct list *list)
 {
@@ -107,7 +138,12 @@ list_rbegin(struct list *list)
 
 /* Returns the element before ELEM in its list.  If ELEM is the
    first element in its list, returns the list head.  Results are
-   undefined if ELEM is itself a list head. */
+   undefined if ELEM is itself a list head. 
+* elem의 이전 요소를 반환합니다. elem이 리스트의 첫 번째 요소인 경우 리스트의 head를 반환합니다.
+* elem이 head이거나 interior인 경우 elem의 prev를 반환합니다.
+* elem이 NULL이 아거나 head인 경우 NULL을 반환합니다.
+*   
+   */
 struct list_elem *
 list_prev(struct list_elem *elem)
 {
@@ -121,12 +157,14 @@ list_prev(struct list_elem *elem)
    reverse order, from back to front.  Here's typical usage,
    following the example from the top of list.h:
 
-   for (e = list_rbegin (&foo_list); e != list_rend (&foo_list);
-   e = list_prev (e))
+	list_rend()는 목록을 뒤에서 앞으로 역순으로 반복하는 데 자주 사용됩니다.  
+	다음은 list.h의 맨 위에 있는 예제에 따른 일반적인 사용법입니다:
+   for (e = list_rbegin (&foo_list); e != list_rend (&foo_list); e = list_prev (e))
    {
    struct foo *f = list_entry (e, struct foo, elem);
    ...do something with f...
    }
+   리스트의 헤드를 반환
    */
 struct list_elem *
 list_rend(struct list *list)
@@ -145,6 +183,7 @@ list_rend(struct list *list)
    {
    ...
    }
+   리스트의 헤드를 반환
    */
 struct list_elem *
 list_head(struct list *list)
@@ -153,7 +192,9 @@ list_head(struct list *list)
 	return &list->head;
 }
 
-/* Return's LIST's tail. */
+/* Return's LIST's tail. 
+* 리스트의 꼬리를 반환
+*/
 struct list_elem *
 list_tail(struct list *list)
 {
@@ -163,7 +204,10 @@ list_tail(struct list *list)
 
 /* Inserts ELEM just before BEFORE, which may be either an
    interior element or a tail.  The latter case is equivalent to
-   list_push_back(). */
+   list_push_back(). 
+* Before 바로 앞에 ELEM을 삽입
+* 꼬리인경우 list_push_back()과 동일
+*/
 void list_insert(struct list_elem *before, struct list_elem *elem)
 {
 	ASSERT(is_interior(before) || is_tail(before));
@@ -176,8 +220,13 @@ void list_insert(struct list_elem *before, struct list_elem *elem)
 }
 
 /* Removes elements FIRST though LAST (exclusive) from their
-   current list, then inserts them just before BEFORE, which may
-   be either an interior element or a tail. */
+*   current list, then inserts them just before BEFORE, which may
+*   be either an interior element or a tail. 
+*    last -> 2, 3, 4, 5에서 2, 3, 4를 옮기는 경우 last는 5
+*    before -> 5 뒤로 옮기는 경우 before는 5
+*    first -> 2, 3, 4 를 옮기는 경우 first는 2
+*   일부 구간을 잘라서 어느 지점 뒤로 옮긴다.
+*/
 void list_splice(struct list_elem *before,
 				 struct list_elem *first, struct list_elem *last)
 {
@@ -201,14 +250,22 @@ void list_splice(struct list_elem *before,
 }
 
 /* Inserts ELEM at the beginning of LIST, so that it becomes the
-   front in LIST. */
+   front in LIST. 
+   * begin은 head의 다음 요소를 반환한다.
+   * head 다음 요소 앞에 elem을 삽입
+   * 결론 맨 앞에 elem을 삽입
+   */
 void list_push_front(struct list *list, struct list_elem *elem)
 {
 	list_insert(list_begin(list), elem);
 }
 
 /* Inserts ELEM at the end of LIST, so that it becomes the
-   back in LIST. */
+   back in LIST. 
+* list_end -> 리스트의 꼬리를 반환
+* 꼬리 앞에 elem을 삽입
+* 리스트 맨 뒤에 elem을 삽입
+*/
 void list_push_back(struct list *list, struct list_elem *elem)
 {
 	list_insert(list_end(list), elem);
@@ -223,16 +280,19 @@ void list_push_back(struct list *list, struct list_elem *elem)
    that a naive loop to remove the elements in a list will fail:
 
  ** DON'T DO THIS **
- for (e = list_begin (&list); e != list_end (&list); e = list_next (e))
- {
- ...do something with e...
- list_remove (e);
- }
+ 	** 잘못 된 예시 **
+ 	for (e = list_begin (&list); e != list_end (&list); e = list_next (e))
+ 	{
+ 		...do something with e...
+ 		list_remove (e);
+ 	}
  ** DON'T DO THIS **
 
  Here is one correct way to iterate and remove elements from a
 list:
-
+** 올바른 제거방법 1 **
+이 방법은 list_remove 함수가 제거된 요소 다음에 오는 요소를 반환하기 때문에 안전합니다. 
+따라서, e를 제거한 후 다음 요소로의 이동이 자연스럽게 이루어집니다.
 for (e = list_begin (&list); e != list_end (&list); e = list_remove (e))
 {
 ...do something with e...
@@ -241,12 +301,16 @@ for (e = list_begin (&list); e != list_end (&list); e = list_remove (e))
 If you need to free() elements of the list then you need to be
 more conservative.  Here's an alternate strategy that works
 even in that case:
-
+** 올바른 제거방법 2 **
+ 리스트가 비어있지 않은 동안 리스트의 맨 앞에서 요소를 제거(list_pop_front)하고, 
+ 그 요소를 처리합니다. list_pop_front 함수는 리스트의 첫 번째 요소를 제거하고 그 요소를 반환합니다. 
+ 이 방법은 요소를 free() 하거나 메모리를 관리해야 할 때 특히 유용
 while (!list_empty (&list))
 {
 struct list_elem *e = list_pop_front (&list);
 ...do something with e...
 }
+* 삭제된 다음 요소를 반환
 */
 struct list_elem *
 list_remove(struct list_elem *elem)
@@ -258,7 +322,11 @@ list_remove(struct list_elem *elem)
 }
 
 /* Removes the front element from LIST and returns it.
-   Undefined behavior if LIST is empty before removal. */
+   Undefined behavior if LIST is empty before removal. 
+*  list_front는 헤드 다음 요소를 반환
+*  *front는 맨 앞 요소의 주소  
+* 맨 앞 요소를 삭제하고 반환
+*/
 struct list_elem *
 list_pop_front(struct list *list)
 {
@@ -268,7 +336,11 @@ list_pop_front(struct list *list)
 }
 
 /* Removes the back element from LIST and returns it.
-   Undefined behavior if LIST is empty before removal. */
+   Undefined behavior if LIST is empty before removal.
+*  list_back은 tail 앞 요소인 맨 뒤 요소를 반환
+*  *back는 맨 뒤 요소의 주소
+*  맨 뒤 요소를 삭제하고 반환
+*/
 struct list_elem *
 list_pop_back(struct list *list)
 {
@@ -278,7 +350,9 @@ list_pop_back(struct list *list)
 }
 
 /* Returns the front element in LIST.
-   Undefined behavior if LIST is empty. */
+   Undefined behavior if LIST is empty. 
+* 리스트의 맨 앞 요소를 반환
+*/
 struct list_elem *
 list_front(struct list *list)
 {
@@ -287,7 +361,10 @@ list_front(struct list *list)
 }
 
 /* Returns the back element in LIST.
-   Undefined behavior if LIST is empty. */
+   Undefined behavior if LIST is empty. 
+ * 리스트 맨뒤 요소를 반환
+ * 꼬리 앞 값  
+*/
 struct list_elem *
 list_back(struct list *list)
 {
@@ -296,7 +373,9 @@ list_back(struct list *list)
 }
 
 /* Returns the number of elements in LIST.
-   Runs in O(n) in the number of elements. */
+   Runs in O(n) in the number of elements. 
+* 리스트의 요소 수를 반환 리스트 내 개수 반환
+*/
 size_t
 list_size(struct list *list)
 {
@@ -308,13 +387,18 @@ list_size(struct list *list)
 	return cnt;
 }
 
-/* Returns true if LIST is empty, false otherwise. */
+/* Returns true if LIST is empty, false otherwise.
+* 리스트가 비어있는지 확인 
+* head의 다음요소가 tail이면 비어있는 리스트 
+*/
 bool list_empty(struct list *list)
 {
 	return list_begin(list) == list_end(list);
 }
 
-/* Swaps the `struct list_elem *'s that A and B point to. */
+/* Swaps the `struct list_elem *'s that A and B point to. 
+* elem a와 b를 바꿈
+*/
 static void
 swap(struct list_elem **a, struct list_elem **b)
 {
@@ -323,7 +407,10 @@ swap(struct list_elem **a, struct list_elem **b)
 	*b = t;
 }
 
-/* Reverses the order of LIST. */
+/* Reverses the order of LIST. 
+* 리스트의 순서를 뒤집음
+* 확인은 안해봄
+*/
 void list_reverse(struct list *list)
 {
 	if (!list_empty(list))
